184fa2d519febb6bfb8a78e8e2508f825c86387b
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-06-12 15:20:14 +0300

    Add Home.js component

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import thumb from '../images/thumb-1.png'
+import './Home.css'
+
+const Home = () => {
+
+    return (
+        <div className='main_hom'>
+            <div className='home_txt'>
+                <h1>Creative way to shawcases your App</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.</p>
+                <button type='button' >Get Started</button>
+            </div>
+            <div className='home_img'>
+                <img src={thumb} alt="thumb-1"></img>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
